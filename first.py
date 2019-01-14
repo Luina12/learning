b115bbf98430785e25e7805afe7673f8b6c2ce2e
paulina jamroz <pk.jamroz@gmail.com>
2019-01-14 19:32:09 +0100

    i first lesson

--- first.py
+++ first.py
@@ -1,3 +1,41 @@
-imie = 'paulina'
-nazwisko = 'jamroz'
-print imie +" "+ nazwisko
+"""name= raw_input('Enter your name:') wczytywanie danych z klawiatury zostaly zapisane w zmiennej name
+surname =  raw_input('Enter your name:')
+age=input('Choose a number')
+print (name + " "+ surname + " " + str(age))konkatynacja- laczenie ze soba kilku strigow w jeden
+"""
+
+"""first= input('number1')#input wczytuje z klawiatury wczytuje inta
+second = input('twonumber')
+if first < second:
+    print second
+elif second < first:# jezeli tak i warunek
+    print first
+else:
+    print 'liczbysarowne'"""
+
+"""str1 = 'a'
+str2 = 'a'
+print(len(str2))
+if len (str1) < len(str2):#metoda lenth zwraca dlugosc stringa
+    print str2
+elif len(str2) < len(str1):
+    print str1
+else:
+    print 'majatylesamoliter'"""
+
+
+
+#convertowanie stringa do inta linia 31 dodawanie do wyniku liczbe 5 i 3
+"""print str(liczba)# dalismy str bo chcialam wyswietlic liczbe jako string
+age= '17'
+int_age = int(age)+5
+print(int_age+3)"""
+suma = 0
+for x in range(10):
+    suma= suma + x
+print suma
+
+
+
+
+
