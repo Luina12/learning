befed829968b212b81c76f5332f2f62f6b998247
paulina jamroz <pk.jamroz@gmail.com>
2019-01-17 20:04:35 +0100

    blebleble

--- first.py
+++ first.py
@@ -31,9 +31,58 @@ age= '17'
 int_age = int(age)+5
 print(int_age+3)"""
 suma = 0
-for x in range(10):
+"""for x in range(10):
     suma= suma + x
-print suma
+print suma"""
+
+imiona= ['piotrek','kasia','magda','piotrek','piotrek']
+"""print imiona
+print imiona[1]
+print len(imiona)
+for imie in imiona:
+    print imie"""
+"""for i in range(len(imiona)):
+    if imiona[i]=='piotrek':
+        print i"""
+
+"""print imiona
+del imiona[0]
+print imiona
+del imiona[2]
+print imiona
+del imiona[2]
+print imiona"""
+
+"""def przywitanie(dupa):
+    print dupa
+
+def pobierz_tekst_przywitania():
+    return 'hello'
+moj_tekst= pobierz_tekst_przywitania()
+przywitanie(moj_tekst)"""
+
+
+
+"""def dodawanie(zmienna1,zmienna2):
+    return zmienna1+zmienna2
+print dodawanie('lalka','pies')"""
+"""def dzielenie(zmienna1,zmienna2):
+    if zmienna1==0 or zmienna2==0:
+        return 'nie moznadzielic przez 0'
+    if zmienna1<0 or zmienna2<0:
+        return 'nie wyswietlaj liczb ujemnych'
+    return zmienna1/zmienna2
+print dzielenie(-5,-6)
+dzielenie= zmienna1+zmienna2
+    suma= dzielenie+x"""
+for x in range(100):
+    if x%3==0 and x%2==0 and x<50:
+        print x
+
+
+
+
+
 
 
 
